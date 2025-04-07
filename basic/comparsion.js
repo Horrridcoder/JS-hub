14cf4b84f0d51d1278f774881595ba4e58cbd681
Horrridcoder <maaz02670@gmail.com>
2025-04-07 18:48:28 +0000

    comparsion.js

---
+++ basic/comparsion.js
@@ -0,0 +1,17 @@
+console.log("2 > 1 = ",2 > 1);
+console.log("2 < 1 = ",2 < 1);
+console.log("2 >= 1 = ",2 >= 1);
+console.log("2 != 1 = ",2 != 1);
+console.log("2 == 1 = ",2 == 1);
+
+// compare string to number
+console.log('"f" > 1',"f" > 1);
+
+// compare null to 0
+console.log("null > 0 =", null > 0);     // false
+console.log("null == 0 =", null == 0);   // false
+console.log("null >= 0 =", null >= 0);   // true
+
+// difference == , ===
+console.log("20" == 20); // == check value not datatype so output is true
+console.log("20" === 20); // === check datatype not value so output is false
