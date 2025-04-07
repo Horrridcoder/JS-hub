777949fe5051d00629f92c66c7f4826a43ae3f2e
Horrridcoder <maaz02670@gmail.com>
2025-04-07 11:36:38 +0000

    container config done

--- basic/variables.js
+++ basic/variables.js
@@ -3,6 +3,12 @@ let accountemail = "horridcoder@google.com"
 var accountpassword = "890123"
 accountcity = "delhi"
 
-// accountid = 2
+// accountid = 2 // not allow
 
 console.log(accountid);
+
+accountemail = "happycoder@gmail.com" // allow
+accountpassword = "123456" // allow
+accountcity = "Mumbai" // allow
+
+console.table([accountid, accountemail, accountpassword, accountcity]) 
