a1f9445e8dfd3e5fe13e8c1c7bf4798a72fc3be5
Horrridcoder <maaz02670@gmail.com>
2025-04-07 18:20:30 +0000

    arthemetic_operators.js

---
+++ basic/arthemetic_operators.js
@@ -0,0 +1,20 @@
+console.log("2 + 2 = ",2+2);
+console.log("2 - 2 = ",2-2);
+console.log("2 / 2 = ",2/2);
+console.log("2 * 2 = ",2*2);
+console.log("2 ** 2 = ",2**2);
+console.log("2 % 2 = ",2%2);
+
+// some examples
+// string + string
+let str1 = "horrid"
+let str2 = " coder"
+console.log(str1 + str2)
+
+// some mindblowing things
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // you think js do 2+2 js never do output is 122
+console.log(1 + 2 + "2"); // 41 becase number should be front and string should be last
+
+console.log(+true);
