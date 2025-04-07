ebcd0ebdf0227b7a4cda2f38d91c8cca9bdc4936
Horrridcoder <maaz02670@gmail.com>
2025-04-07 12:27:37 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; // your code treat as newer version
+
+// alert(3 + 3) // we are use node js not browser
+
+console.log(3 + 3); // use log avoid alert
+
+console.log("Ibrahim");
+
+// Number
+let age = 20;
+
+// String
+let name = "Raju";
+
+// Boolean
+let isFun = true;
+
+// Undefined
+let x;
+
+// Null
+let nothing = null;
+
+// Object
+let info = { name: name, age: age };
+
+// Array
+let list = [1, "hi", isFun];
+
+// Symbol
+let key = Symbol("key");
+
+// BigInt
+let big = 123n;
+
+// Print all
+console.log(age, name, isFun, x, nothing, info, list, key, big);
+
+console.log(typeof big); // use for know to types
