8d3e035d80aa2084e860c4ba3763c6229d98cc13
Horrridcoder <maaz02670@gmail.com>
2025-04-08 11:00:01 +0000

    datatypes-summary

--- basic/datatypes-summary.js
+++ basic/datatypes-summary.js
@@ -17,7 +17,7 @@ console.log("Undefined:", notDefined);
 let arr = [1, 2, 3, 4];                   // array
 let obj = {
     name: "Horrid Coder",
-    age: 22 }; // object
+    age: 22 }; // object 
 function greet() {                        // function
     console.log("Hello, OG!");
 }
@@ -28,3 +28,25 @@ greet();
 
 // typeof
 console.log(typeof obj);
+
+// Stack and Heap memory javascript
+// stack (Primitive Datatypes), heap (Non-Primitive Datatypes)
+// stack mai aapko copy di jati hai variable ki
+let myname = "ibrahim ahmad"
+let mynamecopy = myname
+mynamecopy = "coder"
+console.log(myname);
+console.log(mynamecopy);
+
+// Heap
+// real one ko change krta hai naki copy deta hai
+let userone = {
+    email: "horridcoder@gmail.com",
+    name: "horridcode"
+}
+let usertwo = userone
+usertwo.email = "happycoder@gmail.com"
+usertwo.name = "happycoder"
+
+console.log("userone : ",userone);
+console.log("usertwo : ",usertwo);
